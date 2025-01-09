0d3296e51fbeea1535a6dbc0554601022c23c67a
serevina sherly maulida <101394654+serevinaash@users.noreply.github.com>
2025-01-09 12:04:49 +0700

    header

--- src/App.js
+++ src/App.js
@@ -1,33 +1,20 @@
 import React from "react";
-import { createBrowserHistory } from "history";
-import { Router, Route, Switch } from "react-router-dom";
-import { ToastContainer } from "react-toastify";
-import LandingPage from "pages/LandingPage";
-import DetailsPage from "pages/DetailsPage";
-import Checkout from "pages/Checkout";
-import Example from "pages/Example";
-import NotFound from "pages/404";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";  // Import Routes
+import LandingPage from "pages/LandingPage";  // Ensure you have this component imported
+
+
 
 import "assets/scss/style.scss";
-
-const history = createBrowserHistory({
-  basename: process.env.PUBLIC_URL,
-});
 
 function App() {
   return (
     <div className="App">
-      <Router history={history} basename={process.env.PUBLIC_URL}>
-        <Switch>
-          <Route exact path="/" component={LandingPage} />
-          <Route exact path="/properties/:id" component={DetailsPage} />
-          <Route path="/checkout" component={Checkout} />
-          <Route path="/example" component={Example} />
-          <Route path="*" component={NotFound} />
-        </Switch>
+      <Router>
+        <Routes>  {/* Wrap the Route components with Routes */}
+          <Route path="/" element={<LandingPage />} />  {/* Use 'element' to render the component */}
+        </Routes>
       </Router>
 
-      <ToastContainer></ToastContainer>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,21 +1,15 @@
 import React from "react";
-import ReactDOM from "react-dom";
-
+import ReactDOM from "react-dom/client";  // Updated import for React 18+
 import App from "./App";
-import * as serviceWorker from "./serviceWorker";
-import { Provider } from "react-redux";
-import store from "store";
 
-ReactDOM.render(
+// Create the root and render the app
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById("root")
+    <App />
+  </React.StrictMode>
 );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
