52eed5ec6841dbc0fb41aaa4a29bdaae4f3d793a
serevina sherly maulida <101394654+serevinaash@users.noreply.github.com>
2025-01-14 16:31:41 +0700

    update button

--- src/parts/MostPicked.js
+++ src/parts/MostPicked.js
@@ -25,7 +25,7 @@ export default function MostPicked(props) {
                   />
                 </figure>
                 <div className="meta-wrapper">
-                  <Button
+                  <Button type="link"
                     className="stretched-link d-block text-white"
                     href={`/properties/${item._id}`}
                   >
