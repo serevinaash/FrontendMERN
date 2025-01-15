0ece200f8a62e58661ff5ee3b57e84565e8abb5e
serevina sherly maulida <101394654+serevinaash@users.noreply.github.com>
2025-01-15 12:22:06 +0700

    category

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -5,6 +5,7 @@ import Header from "parts/Header";
 import landingPage from 'json/landingPage.json'
 import Hero from 'parts/Hero';
 import MostPicked from 'parts/MostPicked'
+import Categories from 'parts/Categories'
 
 
 export default class LandingPage extends Component {
@@ -16,13 +17,12 @@ export default class LandingPage extends Component {
 
 
   render() {
-    console.log(this.props);
     return (
       <>
       <Header {...this.props}></Header>
       <Hero refMostPicked={this.refMostPicked} data={landingPage} />
       <MostPicked refMostPicked={this.refMostPicked} data={landingPage.mostPicked} />
-
+      <Categories data={landingPage.categories}></Categories>
 
       </>
     )
