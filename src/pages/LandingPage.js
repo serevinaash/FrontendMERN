c71f47c0ddc354fd5c89331cd8ca9b0ad3dfc035
serevina sherly maulida <101394654+serevinaash@users.noreply.github.com>
2025-01-16 14:15:57 +0700

    footer

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -7,6 +7,7 @@ import Hero from 'parts/Hero';
 import MostPicked from 'parts/MostPicked'
 import Categories from 'parts/Categories'
 import Testimony from 'parts/Testimony';
+import Footer from 'parts/Footer';
 
 export default class LandingPage extends Component {
   constructor(props) {
@@ -24,6 +25,7 @@ export default class LandingPage extends Component {
       <MostPicked refMostPicked={this.refMostPicked} data={landingPage.mostPicked} />
       <Categories data={landingPage.categories}></Categories>
       <Testimony data={landingPage.testimonial}></Testimony>
+      <Footer />
       </>
     )
   }
