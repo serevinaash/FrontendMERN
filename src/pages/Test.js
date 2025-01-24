7a98d9a970df232e060625dbfa2a88e9955e0457
serevina sherly maulida <101394654+serevinaash@users.noreply.github.com>
2025-01-24 17:28:49 +0700

    reactreveal-daterangepicker

---
+++ src/pages/Test.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Fade from "react-reveal/Fade";
+
+export default function Test    () {
+  return (
+    <Fade>
+      <h1>Hello, React Reveal!</h1>
+    </Fade>
+  );
+}
